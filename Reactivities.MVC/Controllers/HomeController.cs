61ba7e275c91772a40bf157ba899222b7c93e4f4
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-12-22 18:50:03 +0300

    Get Activities List From API in MVC

--- Reactivities.MVC/Controllers/HomeController.cs
+++ Reactivities.MVC/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Reactivities.MVC.Models;
+using Reactivities.MVC.ViewModels;
 using System.Diagnostics;
+using System.Text.Json.Serialization;
 
 namespace Reactivities.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        Uri baseAddress = new Uri("http://localhost:5000/api");
+        private readonly HttpClient _client;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HttpClient client)
         {
             _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            List<ActivityViewModel> activityList = new List<ActivityViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Activities/GetActivities").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                activityList = JsonConvert.DeserializeObject<List<ActivityViewModel>>(data);
+            }
+
+
+            return View(activityList);
         }
 
         public IActionResult Privacy()
