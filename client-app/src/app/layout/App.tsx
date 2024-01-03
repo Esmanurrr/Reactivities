8f75fccd8fcc94fd5be91cbbccece6ea25a59fe2
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-04 00:28:41 +0300

    Adding a NavBar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 import { Activity } from './models/activity';
+import NavBar from './NavBar';
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
   return (
     
     <div>
-      <Header as='h2' icon='users' content='Reactivities' />
+      <NavBar/>
       <List>
         {activities.map(activity =>(
           <List.Item key={activity.id}>
