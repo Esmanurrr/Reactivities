5279dfc216be9caf10dcf9d3e430c2b0cdc41752
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-06 02:26:54 +0300

    Posting data to server

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -13,6 +13,7 @@ function App() {
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
 
   useEffect(() => {
@@ -46,11 +47,23 @@ function App() {
   }
 
   function handleCreateOrEditActivity(activity: Activity){
-    activity.id 
-    ? setActivities([...activities.filter(x=>x.id !== activity.id), activity]) 
-    : setActivities([...activities, {...activity, id:uuid()}]);// random id with uuid()
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true);
+    if(activity.id){
+      agent.Activities.update(activity).then(()=>{
+        setActivities([...activities.filter(x=>x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }else{
+      activity.id = uuid();
+      agent.Activities.create(activity).then(()=>{
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    }
   }
 
   function handleDeleteActivity(id:string){
@@ -73,6 +86,7 @@ function App() {
         closeForm={handleFormClose}
         createOrEdit = {handleCreateOrEditActivity}
         deleteActivity = {handleDeleteActivity}
+        submitting = {submitting}
         />
       </Container>
     </>
