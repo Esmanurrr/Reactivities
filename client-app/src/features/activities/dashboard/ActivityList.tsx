2fedc1bc9dbed39f9f44556330d6e3bc8894044c
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-13 22:03:41 +0300

    Styling the list items

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -4,25 +4,21 @@ import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
 
-export default observer( function ActivityList() {
-    const {activityStore} = useStore();
-    const {groupedActivities} = activityStore;
-    
-    
-    return(
+export default observer(function ActivityList() {
+    const { activityStore } = useStore();
+    const { groupedActivities } = activityStore;
+
+
+    return (
         <>
             {groupedActivities.map(([group, activities]) => (
                 <Fragment key={group} >
-                    <Header sub color ='teal'>
+                    <Header sub color='teal'>
                         {group}
                     </Header>
-                    <Segment>
-                        <Item.Group divided>
-                            {activities.map(activity => (
-                                <ActivityListItem key={activity.id} activity={activity} />
-                            ))}
-                        </Item.Group>
-                    </Segment>
+                    {activities.map(activity => (
+                        <ActivityListItem key={activity.id} activity={activity} />
+                    ))}
                 </Fragment>
             ))}
         </>
