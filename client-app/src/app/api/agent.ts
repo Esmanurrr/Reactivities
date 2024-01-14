62ac478da041b4294b21d65ea2b7706a69cab862
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-14 17:39:33 +0300

    Handling 400 errors

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -19,10 +19,21 @@ axios.interceptors.response.use(async response => {
     return response;
 
 }, (error: AxiosError) => {
-    const {data, status} = error.response!;
+    const {data, status} = error.response as AxiosResponse;
     switch (status) {
         case 400:
-            toast.error('bad request')
+            if(data.errors){
+                const modalStateErrors = [];
+                for(const key in data.errors){
+                    if(data.errors[key]){
+                        modalStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modalStateErrors.flat();
+            }else{
+                toast.error(data);
+            }
+
             break;
         case 401:
             toast.error('unauthorised')
