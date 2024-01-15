ce1752553138e75753d951848d5f1bebeb023e3b
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-15 16:43:16 +0300

    created an auth policy

--- Reactivities.API/Controllers/AccountController.cs
+++ Reactivities.API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Reactivities.API.DTOs;
 using Reactivities.API.Services;
@@ -6,6 +7,7 @@ using Reactivities.Domain;
 
 namespace Reactivities.API.Controllers
 {
+    [AllowAnonymous]
     [ApiController]
     [Route("api/[controller]")]
     public class AccountController : ControllerBase
