80ba17578c919e9a3a7e1bf079faeae5cc26cf94
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-01-15 14:35:21 +0300

    configuring Identity in the program.cs

---
+++ Reactivities.API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Reactivities.Domain;
+using Reactivities.Persistence;
+
+namespace Reactivities.API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddIdentityCore<AppUser>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+            })
+            .AddEntityFrameworkStores<DataContext>();
+
+            services.AddAuthentication();
+
+            return services;
+        }
+    }
+}
