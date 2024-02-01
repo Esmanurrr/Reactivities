d3533fb68dba605ccb7ba3dd7a31c2608379389b
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-02-02 00:25:29 +0300

    adding the add photo logic

--- Reactivities.Infrastructure/Photos/PhotoAccessor.cs
+++ Reactivities.Infrastructure/Photos/PhotoAccessor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 using Reactivities.Application.Interfaces;
 using Reactivities.Application.Photos;
 using System;
@@ -11,14 +14,53 @@ namespace Reactivities.Infrastructure.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
-        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        private readonly Cloudinary _cloudinary;
+
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
         {
-            throw new NotImplementedException();
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+            _cloudinary = new Cloudinary( account );
         }
 
-        public Task<string> DeletePhoto(string publicId)
+        public async Task<PhotoUploadResult> AddPhoto(IFormFile file)//to upload file via web I use IFromFile
         {
-            throw new NotImplementedException();
+            if(file.Length > 0)//if there is file
+            {
+                await using var stream = file.OpenReadStream(); //to read file
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill")
+                };
+
+                var uploadResult = await _cloudinary.UploadAsync(uploadParams); //upload image to cloudinary 
+
+                if(uploadResult.Error != null)
+                {
+                    throw new Exception(uploadResult.Error.Message);
+                }
+
+                return new PhotoUploadResult
+                {
+                    PublicId = uploadResult.PublicId,
+                    Url = uploadResult.SecureUrl.ToString()
+                };
+            }
+
+            return null;
+        }
+
+        public async Task<string> DeletePhoto(string publicId)
+        {
+            var deleteParams = new DeletionParams(publicId);//which image will delete?
+
+            var result = await _cloudinary.DestroyAsync(deleteParams);//to delete from cloudinary this image
+
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
