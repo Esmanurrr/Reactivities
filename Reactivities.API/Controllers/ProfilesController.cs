2a3f06c9364e3382c3ba0ccb72f30454788e6698
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-02-02 16:54:04 +0300

    returning user profiles

---
+++ Reactivities.API/Controllers/ProfilesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reactivities.Application.Profiles;
+
+namespace Reactivities.API.Controllers
+{
+    public class ProfilesController : BaseApiController
+    {
+        [HttpGet("{username}")]
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+    }
+}
