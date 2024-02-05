0e5308c4746b179cfa73839f81f66130792af422
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2024-02-05 21:54:16 +0300

    connecting to the hub

--- Reactivities.API/Extensions/ApplicationServiceExtensions.cs
+++ Reactivities.API/Extensions/ApplicationServiceExtensions.cs
@@ -22,7 +22,7 @@ namespace Reactivities.API.Extensions
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
                 });
             });
 
